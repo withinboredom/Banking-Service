4c8431f152436ae0c90df8d0c3b28bdb4ef4b0c5
Rob Landers <landers.robert@gmail.com>
2015-09-08 00:52:54 -0400

    try running selenium

--- BankingService/Selenium/Program.cs
+++ BankingService/Selenium/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 
@@ -14,9 +17,72 @@ namespace Selenium
         // AzureWebJobsDashboard and AzureWebJobsStorage
         static void Main()
         {
-            var host = new JobHost();
-            // The following code ensures that the WebJob will be running continuously
-            host.RunAndBlock();
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomainOnProcessExit;
+            try
+            {
+                var host = new JobHost();
+                // The following code ensures that the WebJob will be running continuously
+                host.RunAndBlock();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to connect to storage --- assuming development?");
+            }
+
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                "binaries\\selenium-server-standalone-2.47.1.jar");
+            var process = new ProcessStartInfo("D:\\Program Files (x86)\\Java\\jdk1.8.0_25\\bin\\java.exe", " -jar " + path)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+
+            var local = new ProcessStartInfo("java.exe", "-jar " + path)
+            {
+                UseShellExecute = false,
+                CreateNoWindow = false
+            };
+
+            try
+            {
+                var proc = Process.Start(process);
+                wait(proc);
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    var dunc = Process.Start(local);
+                    wait(dunc);
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Failed to start selenium");
+                }
+            }
+        }
+
+        private static void CurrentDomainOnProcessExit(object sender, EventArgs eventArgs)
+        {
+            proc_running.Kill();
+        }
+
+        private static Process proc_running;
+
+        static void wait(Process proc)
+        {
+            proc_running = proc;
+
+            if (proc != null)
+            {
+                proc.WaitForExit();
+            }
+            else
+            {
+                throw new Exception("Failed to start selenium");
+            }
+
+            Environment.Exit(1);
         }
     }
 }
