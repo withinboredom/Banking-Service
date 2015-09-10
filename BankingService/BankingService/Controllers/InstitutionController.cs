4e7dcba83ccbfc0a7a45e385ca123ae1e5614af3
Rob Landers <landers.robert@gmail.com>
2015-09-10 01:47:57 -0400

    added institution qualifier

--- BankingService/BankingService/Controllers/InstitutionController.cs
+++ BankingService/BankingService/Controllers/InstitutionController.cs
@@ -6,13 +6,14 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.UI.WebControls;
 using BankingService.Models;
+using BankingService.Models.Banks;
 
 namespace BankingService.Controllers
 {
     public class InstitutionController : ApiController
     {
         [method: HttpPost]
-        public IFlow BeginLogin(string username, string password)
+        public IFlow BeginLogin(string username, string password, Institutions institution)
         {
             return new Flow() {FlowId = Guid.NewGuid(), Id = Guid.NewGuid()};
         }
---
+++ BankingService/BankingService/Models/Banks/Institutions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankingService.Models.Banks
+{
+    public enum Institutions
+    {
+        USAA
+    }
+}
