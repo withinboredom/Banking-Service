3c70b0f323a3c4fc85c1a9c9c7399524f574b5bc
Rob Landers <landers.robert@gmail.com>
2015-10-02 23:05:43 -0400

    Added documentation

--- BankingService/BankLibrary/Banks/BankDriver.cs
+++ BankingService/BankLibrary/Banks/BankDriver.cs
@@ -32,16 +32,35 @@ namespace BankLibrary.Banks
             CreateNextStep("Failed", "Failed to login", false, _currentStep);
         }
 
+        /// <summary>
+        /// Called when a login completes sucessfully
+        /// </summary>
         private void BankOnLoginComplete()
         {
             CreateNextStep("Success", "Logged in", true, _currentStep);
         }
 
+        /// <summary>
+        /// Steps in progress or completed
+        /// </summary>
         public Dictionary<Guid, StepDefinition> Steps { get; private set; }
+
+        /// <summary>
+        /// The current step guid
+        /// </summary>
         private Guid _currentStep;
+
+        /// <summary>
+        /// The bank we are working with
+        /// </summary>
         private readonly Bank _bank;
 
-        public Guid Login(Credentials creds)
+        /// <summary>
+        /// Attempts to login with a set of credentials
+        /// </summary>
+        /// <param name="creds">The credentials to login with</param>
+        /// <returns>A step</returns>
+        public StepDefinition Login(Credentials creds)
         {
             var step = new StepDefinition();
             this._currentStep = Guid.NewGuid();
@@ -50,9 +69,15 @@ namespace BankLibrary.Banks
 
             _bank.Login(creds);
 
-            return this._currentStep;
+            return step;
         }
 
+        /// <summary>
+        /// Performs a step with a given set of credentials
+        /// </summary>
+        /// <param name="step">The step to operate on</param>
+        /// <param name="creds">The credentials to operate with</param>
+        /// <returns>The next step</returns>
         public StepDefinition DoStep(Guid step, Credentials creds)
         {
             var successful = Steps[step].Successful;
@@ -68,6 +93,14 @@ namespace BankLibrary.Banks
             return this.Steps[_currentStep];
         }
 
+        /// <summary>
+        /// Creates a next step
+        /// </summary>
+        /// <param name="field">The field that the next step is for</param>
+        /// <param name="data">The data required</param>
+        /// <param name="success">Has the step been completed</param>
+        /// <param name="thisStep">The current step</param>
+        /// <returns>A step id</returns>
         private Guid CreateNextStep(string field, string data, bool success, Guid thisStep)
         {
             Guid next;
@@ -82,21 +115,34 @@ namespace BankLibrary.Banks
             return next;
         }
 
+        /// <summary>
+        /// Bank requires a user name
+        /// </summary>
         private void BankOnRequireUserName()
         {
             CreateNextStep("UserName", "User Name", false, _currentStep);
         }
 
+        /// <summary>
+        /// Bank requires a question/answer
+        /// </summary>
+        /// <param name="s">The question that needs an answer</param>
         private void BankOnRequireQuestion(string s)
         {
             CreateNextStep("Question", s, false, _currentStep);
         }
 
+        /// <summary>
+        /// Requires a password
+        /// </summary>
         private void BankOnRequirePassword()
         {
             CreateNextStep("Password", "Password", false, _currentStep);
         }
 
+        /// <summary>
+        /// Requires a pin
+        /// </summary>
         private void BankOnRequirePin()
         {
             CreateNextStep("Pin", "Pin", false, _currentStep);
