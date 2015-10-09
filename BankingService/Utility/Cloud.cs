bc87657a1aa81d1d94e4301c1eac3bdd9c309f57
Rob Landers <landers.robert@gmail.com>
2015-10-09 16:54:50 -0400

    Added ref

--- BankingService/Utility/Cloud.cs
+++ BankingService/Utility/Cloud.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 
