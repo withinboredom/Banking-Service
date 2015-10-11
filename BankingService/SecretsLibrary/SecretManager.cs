735c75c3326f54fde7ecd92466f5dea6c7a21c61
Rob Landers <landers.robert@gmail.com>
2015-10-11 01:09:43 -0400

    Add documentation

--- BankingService/SecretsLibrary/SecretManager.cs
+++ BankingService/SecretsLibrary/SecretManager.cs
@@ -15,12 +15,23 @@ namespace SecretsLibrary
         private readonly ICloud _cloud;
         private const string Table = "secrets";
 
+        /// <summary>
+        /// Creates a secret manager with a connection string and cloud
+        /// </summary>
+        /// <param name="connectionString">The connection string</param>
+        /// <param name="cloud">The cloud</param>
         public SecretManager(string connectionString, ICloud cloud)
         {
             _connectionString = connectionString;
             _cloud = cloud;
         }
 
+        /// <summary>
+        /// Creates a secret
+        /// </summary>
+        /// <param name="secretName">A secret</param>
+        /// <param name="secret">More data about the secret</param>
+        /// <returns>The saved secret</returns>
         public ISecret CreateSecret(string secretName, ISecret secret)
         {
             secret.Name = secretName;
@@ -36,12 +47,23 @@ namespace SecretsLibrary
             return _cloud.SetObject(table, store);
         }
 
+        /// <summary>
+        /// Gets a secret by name and version
+        /// </summary>
+        /// <param name="secretName">The name of the secret</param>
+        /// <param name="version">The version of the secret to get</param>
+        /// <returns></returns>
         public ISecret GetSecret(string secretName, int? version)
         {
             var table = _cloud.GetTable(Table, _connectionString);
             return new Secret(StoredSecret.FromTable(table, secretName, version));
         }
 
+        /// <summary>
+        /// Get's a secret by name
+        /// </summary>
+        /// <param name="secretName">The name of the secret</param>
+        /// <returns></returns>
         public ISecret GetSecret(string secretName)
         {
             return GetSecret(secretName, null);
