1825a7b34227b7460341fd6463c334f4afd770f1
Rob Landers <landers.robert@gmail.com>
2015-10-11 16:50:26 -0400

    Update apiversions automatically

---
+++ BankingService/updateVersions.ps1
@@ -0,0 +1,13 @@
+param([String]$version = "1.0.0")
+
+function updateVersion([String] $file) {
+	$bs = Get-Content $file
+	$json = ConvertFrom-Json "$bs"
+	$json.version = $version
+	$bs = ConvertTo-Json $json
+	$bs > $file
+}
+
+updateVersion(".\BankingService\apiapp.json")
+updateVersion(".\KeyVault\apiapp.json")
+updateVersion(".\UserService\apiapp.json")
